ca12e9c137b09de9f84c3fbc7c68dc4d8d6bf77d
AloneSweety <139615872+AloneSweety@users.noreply.github.com>
2024-11-30 10:48:00 +0000

    edit dbconf file

--- public/dbconf.php
+++ public/dbconf.php
@@ -1,22 +1,18 @@
 <?php
-//constant variable
 define('SERVERNAME','127.0.0.1:3306');
 define('USERNAME','root');
-define('PASSWORD','');
+define('PASSWORD','mariadb');
 define('DBNAME','school');
-
-//connect with database
 try{
-	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
-	if(!$connect){
-		die("Connection failed.<br>");
-	}
-	else{
-		//echo("Connected successfully.<br>");
-	}
+    $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+    if(!$connect){
+        die("Connection failed".mysqli_connect_error());
+    }
+    else{
+        //echo "Connected successfully";
+    }
 }
 catch(Exception $e){
-	die($e->getMessage());
+    die($e->getMessage());
 }
-//echo "abc.<br>";
 ?>
